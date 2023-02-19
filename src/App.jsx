40663579d6ce60f8c6c7a9379883298f3511ed1c
Jodarini <rivasjose9428@gmail.com>
2023-02-19 00:06:28 -0500

    Added responsiveness when dragging over other items and added rounded borders for ui

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,36 @@ import "./App.css";
 import { v4 as uuidv4 } from "uuid";
 
 let dbMock = [
-	{ name: "Task 1", isChecked: false, id: 0 },
-	{ name: "Task 2", isChecked: true, id: 1 },
-	{ name: "Task 3", isChecked: true, id: 2 },
-	{ name: "Task 4", isChecked: false, id: 3 },
-	{ name: "Task 5", isChecked: false, id: 4 },
+	{
+		name: "Task 1",
+		isChecked: false,
+		id: 0,
+		isDragged: false,
+	},
+	{
+		name: "Task 2",
+		isChecked: true,
+		id: 1,
+		isDragged: false,
+	},
+	{
+		name: "Task 3",
+		isChecked: true,
+		id: 2,
+		isDragged: false,
+	},
+	{
+		name: "Task 4",
+		isChecked: false,
+		id: 3,
+		isDragged: false,
+	},
+	{
+		name: "Task 5",
+		isChecked: false,
+		id: 4,
+		isDragged: false,
+	},
 ];
 
 export default function App() {
@@ -15,6 +40,12 @@ export default function App() {
 	const [todoList, setTodoList] = useState(dbMock);
 	const [filter, setFilter] = useState("all");
 	const [draggedTodo, setDraggedTodo] = useState();
+	const [isDarkMode, setIsDarkMode] = useState("dark");
+	const [isDragged, setIsDragged] = useState(true);
+
+	const handleChangeMode = () => {
+		setIsDarkMode(isDarkMode === "light" ? "dark" : "light");
+	};
 
 	const newTodo = () => {
 		let newTodo = {
@@ -73,16 +104,40 @@ export default function App() {
 
 	const handleDragStart = (e, todo) => {
 		setDraggedTodo(todo);
+		setIsDragged(!isDragged);
+		todo.isDragged = isDragged;
 	};
 
-	const handleDragOver = e => {
+	const handleDragOver = (e, todo) => {
 		e.preventDefault();
 	};
 
+	const handleDragEnd = (e, todo) => {
+		setIsDragged(!isDragged);
+		todo.isDragged = isDragged;
+	};
+
+	const handleTouchStart = (e, todo) => {
+		setDraggedTodo(todo);
+	};
+	const handleTouchEnd = (e, todo) => {
+		console.log(todo);
+		handleDrop(todo);
+	};
+
 	const handleDrop = target => {
+		// Here is where the add query to db would be
+	};
+
+	const handleDragEnter = target => {
+		if (target === draggedTodo) {
+			console.log("returning before");
+			return;
+		}
 		const newTodoList = [...todoList];
 		const indexOfTarget = newTodoList.indexOf(target);
 		const indexOfdraggedTodo = newTodoList.indexOf(draggedTodo);
+
 		if (indexOfdraggedTodo > indexOfTarget) {
 			newTodoList.splice(indexOfdraggedTodo, 1);
 			newTodoList.splice(indexOfTarget, 0, draggedTodo);
@@ -90,16 +145,21 @@ export default function App() {
 			newTodoList.splice(indexOfdraggedTodo, 1);
 			newTodoList.splice(indexOfTarget, 0, draggedTodo);
 		} else return;
+
 		setTodoList(newTodoList);
 	};
+
 	return (
-		<div className="App flex flex-col items-center">
-			<h1 className="text 3xl font-bold color">ToDo app</h1>
-			<div className="flex flex-col bg-slate-700 md:w-2/6">
+		<div className="flex flex-col items-center">
+			<h1 className="text 3xl color font-bold">ToDo app</h1>
+			<button className="" onClick={() => handleChangeMode()}>
+				{isDarkMode}
+			</button>
+			<div className="flex flex-col rounded-3xl bg-slate-700 md:w-2/6">
 				<form onSubmit={handleSubmit} className="z-50">
 					<input
 						type="textfield"
-						className="bg-slate-700 text-slate-200 p-4 w-full border-b border-gray-600"
+						className="w-full rounded-t-3xl border-b border-gray-600 bg-slate-700 p-4 text-slate-200"
 						autoFocus
 						placeholder="Create a new todo..."
 						onChange={e => setName(e.target.value)}
@@ -112,27 +172,28 @@ export default function App() {
 				<ul>
 					{filteredList.length > 0
 						? filteredList.map(todo => (
-								<li
-									className="flex flex-row bg-slate-700 text-slate-200 w-full cursor-pointer border-b border-gray-600"
+								<ToDo
 									key={todo.id}
-								>
-									<ToDo
-										name={todo.name}
-										handleOnClick={() => handleOnClick(todo.id)}
-										handleOnKeyDown={e => handleOnKeyDown(e, todo.id)}
-										id={todo.id}
-										handleDelete={() => handleDelete(todo.id)}
-										status={todo.isChecked}
-										handleDragStart={e => handleDragStart(e, todo)}
-										handleDragOver={e => handleDragOver(e)}
-										handleDrop={() => handleDrop(todo)}
-									/>
-								</li>
+									name={todo.name}
+									handleOnClick={() => handleOnClick(todo.id)}
+									handleOnKeyDown={e => handleOnKeyDown(e, todo.id)}
+									id={todo.id}
+									handleDelete={() => handleDelete(todo.id)}
+									status={todo.isChecked}
+									isDragged={todo.isDragged}
+									handleDragStart={e => handleDragStart(e, todo)}
+									handleDragEnd={e => handleDragEnd(e, todo)}
+									handleTouchStart={e => handleDragStart(e, todo)}
+									handleDragOver={e => handleDragOver(e, todo)}
+									handleDrop={() => handleDrop(todo)}
+									handleTouchEnd={() => handleTouchEnd(todo)}
+									handleDragEnter={() => handleDragEnter(todo)}
+								/>
 						  ))
 						: "No todo in the list"}
 				</ul>
 
-				<div className="flex flex-row gap-2 justify-center p-2 border-t border-gray-600">
+				<div className="flex flex-row justify-center gap-2 border-t border-gray-600 p-2">
 					<button
 						onClick={() => handleFilter("all")}
 						className={`hover:text-gray-300 ${
@@ -170,6 +231,14 @@ export default function App() {
 	);
 }
 
+function Skeleton() {
+	return (
+		<div className="group relative flex w-full flex-row items-center justify-center align-middle hover:cursor-pointer hover:bg-slate-600 ">
+			Hello world
+		</div>
+	);
+}
+
 function ToDo({
 	name,
 	handleOnClick,
@@ -177,65 +246,80 @@ function ToDo({
 	id,
 	handleDelete,
 	status,
+	isDragged,
 	handleDragStart,
+	handleTouchStart,
 	handleDrop,
 	handleDragOver,
+	handleTouchEnd,
+	handleDragEnter,
+	handleDragEnd,
 }) {
 	return (
 		<>
-			<div
-				draggable
-				className="hover:bg-slate-600 hover:cursor-pointer w-full relative flex flex-row align-middle items-center justify-center group "
-				onDragStart={handleDragStart}
-				onDragOver={handleDragOver}
-				onDrop={handleDrop}
+			<li
+				className={`flex w-full cursor-pointer flex-row border-b border-gray-600 bg-slate-700 text-slate-200 ${
+					isDragged && "bg-slate-800"
+				}`}
 			>
-				<input
-					type="checkbox"
-					onClick={handleOnClick}
-					id={id}
-					className="mr-6 align-baseline hidden"
-				></input>
-
-				<label
-					tabIndex="0"
-					onKeyDown={handleOnKeyDown}
-					htmlFor={id}
-					className="w-full text-left p-6 hover:cursor-pointer pl-12 peer"
+				<div
+					draggable
+					className={`group relative flex w-full flex-row items-center justify-center align-middle hover:cursor-pointer hover:bg-slate-600 `}
+					onTouchStart={handleTouchStart}
+					onTouchEnd={handleTouchEnd}
+					onDragStart={handleDragStart}
+					onDragEnd={handleDragEnd}
+					onDragOver={handleDragOver}
+					onDrop={handleDrop}
+					onDragEnter={handleDragEnter}
 				>
-					{name}
-				</label>
+					<input
+						type="checkbox"
+						onClick={handleOnClick}
+						id={id}
+						className="mr-6 hidden align-baseline"
+					></input>
 
-				<label
-					htmlFor={id}
-					className={`${
-						status
-							? "bg-gradient-to-b from-blue-500 to-purple-500"
-							: "border-gray-600 border-2 group-hover:border-gray-400"
-					} rounded-full h-8 w-8 flex items-center justify-center absolute left-2 hover:cursor-pointer`}
-				>
-					<svg
-						className={`${status ? "block" : "hidden"}`}
-						viewBox="0 0 24 24"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
+					<label
+						tabIndex="0"
+						onKeyDown={handleOnKeyDown}
+						htmlFor={id}
+						className="peer w-full p-6 pl-12 text-left hover:cursor-pointer"
 					>
-						<path
-							d="M6 14L11 18L19 8"
-							stroke="white"
-							strokeWidth="2"
-							strokeLinecap="round"
-							strokeLinejoin="round"
-						/>
-					</svg>
-				</label>
-				<label
-					className="md:hidden rounded-full hover:bg-slate-400 right-2 absolute group-hover:block cursor-pointer peer-focus:block focus:block"
-					onClick={handleDelete}
-				>
-					&times;
-				</label>
-			</div>
+						{name}
+					</label>
+
+					<label
+						htmlFor={id}
+						className={`${
+							status
+								? "bg-gradient-to-b from-blue-500 to-purple-500"
+								: "border-2 border-gray-600 group-hover:border-gray-400"
+						} absolute left-2 flex h-8 w-8 items-center justify-center rounded-full hover:cursor-pointer`}
+					>
+						<svg
+							className={`${status ? "block" : "hidden"}`}
+							viewBox="0 0 24 24"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M6 14L11 18L19 8"
+								stroke="white"
+								strokeWidth="2"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							/>
+						</svg>
+					</label>
+					<label
+						className="absolute right-2 cursor-pointer rounded-full hover:bg-slate-400 focus:block group-hover:block md:hidden"
+						onClick={handleDelete}
+					>
+						&times;
+					</label>
+				</div>
+			</li>
 		</>
 	);
 }
